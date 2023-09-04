247618608d1ad12bbb81e295d81939d16d0444d3
your_github_username <your_github_email>
2023-09-05 00:56:34 +0330

    feat:make profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,7 @@ app_name ='accounts'
 urlpatterns= [
     path('register/' , views.UserRegisterView.as_view() , name='user_register') ,
     path('login/' , views.UserLoginView.as_view() , name='user_login'),
-    path('logout/', views.user_logout , name='user_logout'),
+    path('logout/', views.UserLogoutView.as_view(), name='user_logout'),
+    path('profile/<int:user_id>/' , views.UserProfileView.as_view() , name='user_profile'),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,24 +4,28 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate ,login ,logout
 from django.views import View
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 class UserRegisterView(View):
     form_class= UserRegisterationForm
     template_name='accounts/register.html'
 
-    def get(self, request, *args, **kwargs):
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home:home')
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request):
         form = self.form_class()
         return render(request, self.template_name, {"form": form})
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         form = self.form_class(request.POST)
         if form.is_valid():
             cd=form.cleaned_data
-            print(cd['username'])
-            print(cd['password1'])
-            User.objects.create_user(cd['username'],cd['email'],cd['password2'] )
+            print(cd)
+            User.objects.create_user(cd['username'], cd['email'], cd['password1'])
             messages.success(request , "User registration successful" , "Success")
             return redirect('home:home')
         return render(request, self.template_name , {"form": form}) #this is for error that dont complete the form
@@ -31,17 +35,22 @@ class UserLoginView(View):
     form_class = UserLoginForm
     template_name = 'accounts/login.html'
 
-    def get(self, request, *args, **kwargs):
-        form = self.form_class()
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home:home')
+        return super().dispatch(request, *args, **kwargs)
+    
+    def get(self, request ):
+        form = self.form_class()#make instance from class
         return render(request, self.template_name, {"form": form})
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request ):
         form = self.form_class(request.POST)
         if form.is_valid():
             cd=form.cleaned_data
             user = authenticate(request , username=cd['username'] ,password=cd['password'])
             if user is not None :
-                login(request , user)
+                login(request , user)# user in request
                 messages.success(request , "User Login successful" , "Success")
                 return redirect('home:home')
             else:
@@ -51,10 +60,18 @@ class UserLoginView(View):
 
 
 
+class UserLogoutView(LoginRequiredMixin ,  View):
+    def get(self, request):
+        logout(request)
+        messages.success(request , 'logged out successfully ' ,'success')
+        return redirect('home:home')
 
 
 
-def user_logout(request):
-    logout(request)
-    messages.success(request , 'logged out successfully ' ,'success')
-    return redirect('home:home')
+
+class UserProfileView(LoginRequiredMixin , View):
+    def get(self, request , user_id):
+        print(user_id)
+        user=User.objects.get(id=user_id)
+        
+        return render(request , 'accounts/profile.html',{'user':user})
